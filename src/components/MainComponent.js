2b5ca164fd1390f140fd7611c298076de1df56d5
Ramya <ramyanaryn@gmail.com>
2021-03-21 23:16:53 +0800

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import Home from './HomeComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import Header from './HeaderComponent';
+import Contact from './ContactComponent';
 import Footer from './FooterComponent';
-import {Switch, Route, Redirect} from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-        dishes: DISHES,
-        
+      dishes: DISHES,
+      comments:COMMENTS,
+      promotions:PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
@@ -20,21 +26,26 @@ class Main extends Component {
 
   render() {
     const HomePage = () => {
-      return(
-          <Home/>
+      return (
+        <Home dish={this.state.dishes.filter((dish)=>dish.featured)[0]}
+        promotion={this.state.promotions.filter((promo)=>promo.featured)[0]}
+        leader={this.state.leaders.filter((leader)=>leader.featured)[0]}
+        />
+
       );
     }
 
     return (
       <div>
-        <Header/>
+        <Header />
         <Switch>
-        <Route path='/home' component={HomePage} />
-              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
-              <Redirect to="/home" />
+          <Route path='/home' component={HomePage} />
+          <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+          <Route exact path='/contactus' component={Contact} />
+          <Redirect to="/home" />
         </Switch>
-        
-        <Footer/>
+
+        <Footer />
       </div>
     );
   }
